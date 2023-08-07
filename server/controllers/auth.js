27e3d44b75a2674ecbaf270b943cf697e944e229
Phillip Avila <Anthonyavilz@gmail.com>
2023-08-06 21:19:39 -0500

    small updates

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -20,8 +20,7 @@ module.exports = {
                 const salt = bcrypt.genSaltSync(10)
                 const hash = bcrypt.hashSync(password, salt)
                 const newUser = await User.create({username: username, hashedPass: hash})
-                console.log(newUser)
-                console.log('user created')
+                console.log('user created', newUser)
                 const token = createToken(newUser.dataValues.username, newUser.dataValues.id)
                 const exp = Date.now() + 1000 * 60 * 60 * 48
                 res.status(200).send({username: newUser.dataValues.username, userId: newUser.dataValues.id, token, exp})
